72946bb8f96468a928e91c6d6058ca85c64c3a49
Subrat Shukla <subhrat9928@gmail.com>
2023-07-14 19:13:59 +0530

    adding new file Diff.cpp

---
+++ Diff.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 2, diff=0;
+    diff = a - b;
+    cout<<"Difference of "<<a<< " and "<<b << " is: "<<diff;
+    cout<<"Process completed";
+    return 0;
+}
