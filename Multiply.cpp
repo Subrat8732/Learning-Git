ffa89d7265aa4eb7c6cb37544924e88e47cc77e0
Subrat Shukla <subhrat9928@gmail.com>
2023-07-14 19:27:22 +0530

    added Multiply.java

---
+++ Multiply.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 5, multy=0;
+    multy = a*b;
+    cout<<"Multiply of "<<a<< " and "<<b << " is: "<<multy;
+    cout<<"Process completed";
+    return 0;
+} 
